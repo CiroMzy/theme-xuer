70d42ef7cf9b0d2f14648799bc6e835cc716fcb6
mazhaoyang <414739610@qq.com>
2022-09-27 15:59:37 +0800

    header 调整

--- assets/theme.js
+++ assets/theme.js
@@ -46,3 +46,18 @@ var Slideshow = class extends BaseHTMLElement {
   }
 };
 window.customElements.define("xuer-slideshow", Slideshow);
+
+var HeaderMenu = class extends BaseHTMLElement {
+  connectedCallback () {
+    this.bindMouseEvent()
+  }
+  bindMouseEvent () {
+    $(this).find('.menu-level-1').hover(function() {
+      $(this).addClass('open')
+    }, function() {
+      $(this).removeClass('open')
+    })
+  }
+
+};
+window.customElements.define("xuer-header-menu", HeaderMenu);
