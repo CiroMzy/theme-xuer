9e553eeeaef1df12552c9f7910beb34500cc4df7
mazhaoyang <414739610@qq.com>
2022-10-17 19:57:11 +0800

    form 添加 radio

--- themeJs/localization-form.js
+++ themeJs/localization-form.js
@@ -5,14 +5,29 @@
 var LocalizationForm = class extends BaseHTMLElement {
   connectedCallback () {
     this.bindMouseEvent()
-
   }
   bindMouseEvent () {
-    var $container = $('[xuer-list-selector]')
-    $container.find('.localization-select-item').click(function() {
-      var val = $(this).data('value')
-      $container.find('[name=locale_code]').val(val)
+    var _this = this
+    this.$container = $('[xuer-list-selector]')
+    this.$container.find('.localization-select-item').click(function() {
+      _this.resetChecked()
     })
+  }
+  resetChecked() {
+    var val = $(this).find('input[name=language]:checked').val()
+    this.$container.find('[xuer-list]').children().each(function(idx, el) {
+      console.log($(this).data('value'), val);
+      if ($(this).data('value') === val) {
+        $(this).addClass('checked')
+          .find('.xuer-radio-wrapper').addClass('xuer-radio-wrapper-checked')
+      } else {
+        $(this).removeClass('checked')
+          .find('.xuer-radio-wrapper').removeClass('xuer-radio-wrapper-checked')
+      }
+    })
+    this.$container.find('[name=locale_code]').val(val)
+
+
   }
 };
 window.customElements.define("xuer-localization-form", LocalizationForm);
