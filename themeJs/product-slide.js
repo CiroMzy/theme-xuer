fca49a7d09eb462b5de17022d2bf5452e0b9b447
mazhaoyang <414739610@qq.com>
2022-10-25 16:04:02 +0800

    featured product 调整

--- themeJs/product-slide.js
+++ themeJs/product-slide.js
@@ -1,30 +1,32 @@
 // product-slide
 var ProductSlide = class extends BaseHTMLElement {
   connectedCallback () {
-    
     console.log(this.$container);
+    this.initSwiper()
     // if (this.$container.data(''))
   }
   initSwiper () {
-    
-    // var swiper = new Swiper(".mySwiper", {
-    //   loop: true,
-    //   spaceBetween: 10,
-    //   slidesPerView: 4,
-    //   freeMode: true,
-    //   watchSlidesProgress: true,
-    // });
-    // var swiper2 = new Swiper(".mySwiper2", {
-    //   loop: true,
-    //   spaceBetween: 10,
-    //   navigation: {
-    //     nextEl: ".swiper-button-next",
-    //     prevEl: ".swiper-button-prev",
-    //   },
-    //   thumbs: {
-    //     swiper: swiper,
-    //   },
-    // });
+    var mainId = this.$container.find('[swiper-main]').attr('id')
+    var thumbnailId = this.$container.find('[swiper-thumbnail]').attr('id')
+
+    var swiperMain = new Swiper(`#${thumbnailId}`, {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    var swiperThumb = new Swiper(`#${mainId}`, {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiperMain,
+      },
+    });
   }
 };
 window.customElements.define("xuer-product-slide", ProductSlide);
