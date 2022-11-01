8a1ce4b544e600ab61f4669e83a7e945e94d9b6f
mazhaoyang <414739610@qq.com>
2022-11-01 21:02:17 +0800

    main-login

--- dist/assets/theme.js
+++ dist/assets/theme.js
@@ -316,6 +316,37 @@ $(function () {
       theme.drawer.open($(this).data('action-type'))
   })
 })
+// FormInput
+var FormInput = class extends BaseHTMLElement {
+  connectedCallback () {
+    this.$input = $(this.$container.find('[xuer-input]'))
+    this.focusClass = 'xuer-focus'
+    this.bindChange()
+  }
+  bindChange () {
+    var _this = this
+    this.$input.bind("input propertychange",function(event){
+      var val = _this.getCurInputVal()
+      _this.troggleFocus(val)
+    });
+  }
+
+  troggleFocus (val) {
+    if (!val.length) {
+      this.$input.removeClass(this.focusClass)
+    } else {
+      if (!this.$input.hasClass(this.focusClass)) {
+        this.$input.addClass(this.focusClass)
+      }
+    }
+  }
+
+  getCurInputVal () {
+    return this.$input.val()
+  }
+};
+window.customElements.define("xuer-form-input", FormInput);
+
 var HeaderMenu = class extends BaseHTMLElement {
   connectedCallback () {
     this.bindMouseEvent()
