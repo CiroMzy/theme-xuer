48521738949cc817040a8eb91088aa23fe1b4e07
mazhaoyang <414739610@qq.com>
2023-02-21 18:59:18 +0800

    调整首页模块动画

--- themeJs/header-menu.js
+++ themeJs/header-menu.js
@@ -5,8 +5,16 @@ var HeaderMenu = class extends BaseHTMLElement {
   bindMouseEvent () {
     $(this).find('.menu-level-1').hover(function() {
       $(this).addClass('open')
+      const $children = $(this).find('.menu-level-2')
+      $children.each(function(i, el) {
+        $(el).addClass(`xuer-animated xuer-animate_duration-faster xuer-animate_delay${i + 1} xuer-animate_fadeInUp`)
+      })
     }, function() {
       $(this).removeClass('open')
+      const $children = $(this).find('.menu-level-2')
+      $children.each(function(i, el) {
+        $(el).removeClass(`xuer-animated xuer-animate_duration-faster xuer-animate_delay${i + 1} xuer-animate_fadeInUp`)
+      })
     })
   }
 
